f53e3346403a1527f6912b84c355673fa1a9eaa1
mkdk09 <mkdk099@gmail.com>
2020-12-26 15:01:58 +0900

    ブログを投稿できるように実装

--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,10 @@ export default new Vuex.Store({
   mutations: {
     FETCH_BLOGS(state, blogs) {
       state.blogs = blogs
+    },
+    ADD_BLOG(state, blog) {
+      const blogs = state.blogs.concat(blog)
+      state.blogs = blogs
     }
   },
   actions: {
@@ -20,6 +24,12 @@ export default new Vuex.Store({
           commit('FETCH_BLOGS', res.data)
         }) 
         .catch(e => console.log(e))
-    }
+    },
+    async addBlog({ commit }, blog) {
+      const res = await axios().post('/blogs', blog)
+      const savedBlog = res.data
+      commit('ADD_BLOG', savedBlog)
+      return savedBlog
+    },
   }
 })
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -3,14 +3,9 @@
     <h1>Blogs</h1>
     <v-row>
       <v-col cols="4">
-        <form>
-          <v-text-field v-model="blog.title" label="Title"></v-text-field>
-          <v-textarea v-model="blog.body" label="Body"></v-textarea>
-
-          <v-btn class="mr-4" @click="onSubmit">Create</v-btn>
-        </form>
+        <AddBlog />
       </v-col>
-      <v-cols cols="8">
+      <v-col cols="8">
         <table>
           <tr>
             <th>Title</th>
@@ -24,17 +19,21 @@
             <td>Destroy</td>
           </tr>
         </table>
-      </v-cols>
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import AddBlog from './AddBlog'
 
 export default {
+  components: {
+    AddBlog
+  },
   computed: {
-    ...mapState(['blogs'])
+    ...mapState(['blogs']),
   },
   data () {
     return {
