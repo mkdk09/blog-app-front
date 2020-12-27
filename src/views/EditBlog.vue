473121a09f2f325f71aead5cfba021c283ae156a
mkdk09 <mkdk099@gmail.com>
2020-12-27 14:34:42 +0900

    validationを実装

--- src/views/EditBlog.vue
+++ src/views/EditBlog.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h2>Editing Blog</h2>
-    <form>
+    <v-form ref="checkForm">
       <v-text-field
         v-model="blog.title"
         label="Title"
+        :rules="[required('Title')]"
       ></v-text-field>
       <v-textarea
         v-model="blog.body"
         label="Body"
+        :rules="[required('Body')]"
       ></v-textarea>
 
       <v-btn class="mr-4" @click="updateBlog">Update</v-btn>
-    </form>
+    </v-form>
   </div>
 </template>
 
@@ -24,12 +26,21 @@ export default {
     ...mapState(['blogs']),
     blog() {
       return this.blogs.find(blogId => blogId.id == this.$route.params.id) || {}
+    },
+  },
+  data () {
+    return {
+      required(propertyType) { 
+        return v => v && v.length > 0 || `You must input a ${propertyType}`
+      }
     }
   },
   methods: {
     async updateBlog() {
-      const blog = await this.$store.dispatch('editBlog', this.blog) // ①
-      this.$router.push({ name: 'show-blog', params: { id: blog.id }})
+      if (this.$refs.checkForm.validate()) {
+        const blog = await this.$store.dispatch('editBlog', this.blog) // ①
+        this.$router.push({ name: 'show-blog', params: { id: blog.id }})
+      }
     }
   }
 }
